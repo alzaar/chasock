ad0ec3bb5a9dc9fcd06df447e5730ec35a194c44
Ali Zahir <alizahir@live.com>
2019-05-22 22:51:29 -0400

    Setup Socket.io

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ io.on('connection', (socket) => {
   console.log('User connected.')
   socket.on('disconnect', () => {
     console.log('User disconnected.');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   })
 });
 //Server Start
