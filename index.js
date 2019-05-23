aaa54ce42c16741f56e61776d41144c19dcdf506
Ali Zahir <alizahir@live.com>
2019-05-22 22:41:46 -0400

    Setup Socket.io

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const http = require('http').Server(app);
 //Routes
 const mainRoute = require('./routes');
 //Middleware
 const expressLayouts = require('express-ejs-layouts');
+const io = require('socket.io')(http);
 //Middleware Setup
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.use('/', mainRoute);
+//IO Setup
+io.on('connection', (socket) => {
+  console.log('User connected.')
+  socket.on('disconnect', () => {
+    console.log('User disconnected.');
+  })
+});
 //Server Start
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+http.listen(PORT, () => console.log(`Listening at ${PORT}`));
